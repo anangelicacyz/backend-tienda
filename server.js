a50ab7845f30137d0faf73be409e28ed8712d848
Ñaña <anangeluxd@gmail.com>
2023-02-07 11:41:31 -0500

    agrego consultas mysql para todos los endpoints

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // importar libreria:toda la libreria que se llama express
 //se metió en la variable const express
 const express = require("express")
-const db = require("./db")
+//const db = require("./db")
+const mysql= require("mysql")
 /* ejemplo
 const ana = require("ana")*/
 
@@ -9,157 +10,218 @@ const ana = require("ana")*/
 //variable app 
 const app = express()
 app.use(express.json())
+
+const connection= mysql.createConnection({
+    host: "localhost", 
+    user: "root",
+    password: "1234",
+    database: "tienda"
+})
+
+connection.connect()
+
 //request, response
 
 //endpoint GET /productos
-
-
 app.get("/productos", (req, res)=>{
 
-    res.json(db.productos)
-
+    connection.query(`select * from products`, (error, result, fields)=> {
+        console.log( result)
+        
+        res.json({
+            products: result 
+        })
+    })
 })
 //endpoint buscar por id
 app.get("/productos/:id", (req, res)=>{
     //obtener variable id
     const idProduct = req.params.id
     
-    const resultadoBusqueda = db.productos.find(producto => producto.id === Number(idProduct))
-    res.json(resultadoBusqueda)
-
+    connection.query(`select * from products where id = ${idProduct}`, (error, result, fields)=>{
+        if(result.length=== 0){
+            res.status(404).json( {
+                message: `ID not found`
+            })
+            return
+        }
+        res.json({ product: result[0] })
+    })
 })
 // endpoint delete
 app.delete("/productos/:id", (req, res)=>{
     //obtener variable id
     const idProduct = req.params.id
     
-    const validationProduct = db.productos.find(producto => producto.id === Number(idProduct))
+    connection.query(`delete from products where id= ${idProduct}`, (error, result, fields)=>{
 
-    if(!validationProduct){
-            res.status(404).json({
-                message: "ID not found", code: "404"
-            })
-            return
-        }
+    
+        res.json({
 
-    db.productos = db.productos.filter(producto => producto.id !== validationProduct.id)
-    res.json(db.productos)
+            success: result.affectedRows>0
+        })
+    })
+
 
 })
 app.post("/productos", (req, res)=>{
-    const name= req.body.name
-    const price=req.body.price
-    const units= req.body.units || 0
-        if(!name || !price){
+
+    const {name, description, value, units, createdByUserId, providerId} = req.body
+    const createdAt= new Date()
+
+        if(!name || !value || !units || !createdByUserId || !providerId){
             res.status(400).json({
-                message: "Los campos Name y Price  son obligatorios"
+                message: "Todos los campos son obligatorios"
             })
         }
-    const id = db.productos.length + 1 
+        let productoNuevo = {
+             name , description, units, value, createdAt, createdByUserId, providerId
+        }   
+        //INSERT INTO products (name, description, units, value) VALUES (`${name}`, `${description}`, ${units}, ${value})
+        connection.query(`insert into products set ?`, productoNuevo, (error, result, fields)=>{
 
-    let productoNuevo = {
-        id: id , name: name , price: price, units: units 
-    }
+            res.json({
+                success: result.affectedRows !== 0
+            })
+            
+        } )
 
-    db.productos.push(productoNuevo)
-    res.json(db.productos)
+
 }
 )
 app.put("/productos/:idProducto" , (req, res)=>{
     const idProducto= Number(req.params.idProducto)
-    const indice = db.productos.findIndex(producto => producto.id === idProducto)
-    if(indice<0){
-        res.status(404).json(
-            {
-                message : "No existe elemento para actualizar"
+    const updatedAt= new Date()
+    const {name, description, value, units, updatedByUserId, providerId} = req.body
+    connection.query(
+        `update products set name= ?, description=?, value=?, units=?, updatedAt=?, updatedByUserId=?, providerId=? where id=?`,
+        [name, description, value, units, updatedAt, updatedByUserId, providerId, idProducto], 
+        (error, result, fields)=>{
+            console.log(result)
+            if(result.affectedRows===0){
+            res.status(404).json({
+                message: "We cannot update because the ID was not found",
+            })
+            return
             }
-        )
-    }
-    const name= req.body.name
-    const price= req.body.price
-    const units = req.body.units
-
-    let productoActualizado = {
-        name,  price, units, id: idProducto
-    }
-    db.productos [indice] =productoActualizado
-    res.json( db.productos)
-
+            res.json({
+                result
+            })
+    })
+        
 })
 
 app.get("/usuarios", (req,res)=>{
-    res.json(db.usuarios)
+    
+    connection.query(`SELECT * FROM usuarios`, (error, result, fields)=>{
+
+        res.json({
+            result
+        })
+    })
+    
 })
 app.get("/usuarios/:idUser", (req, res) =>{
-    const idUsuario= Number(req.params.idUser)
+    const idUser= Number(req.params.idUser)
 
-    const ResultadoUsuario = db.usuarios.find(usuario => usuario.id === idUsuario)
-    res.json(ResultadoUsuario)
-})
-app.delete("/usuarios/:idUser", (req, res) =>{
-    const idUser = Number(req.params.idUser)
-    ValidationUser = db.usuarios.find(usuario => usuario.id === idUser)
-        if(!ValidationUser){
+    connection.query(`SELECT * FROM usuarios WHERE id= ${idUser}`, (error, result, fields)=>{
+        if(result.length ===0){
             res.status(404).json({
                 message: "ID not found"
             })
+            return
         }
-    db.usuarios = db.usuarios.filter(usuario => usuario.id !== ValidationUser.id)
-    res.json(db.usuarios)
+        res.json({
+            user: result[0]
+        })
+       
+    })
+    
+})
+app.delete("/usuarios/:idUser", (req, res) =>{
+    const idUser = Number(req.params.idUser)
+
+    connection.query(`DELETE FROM usuarios WHERE id= ${idUser}`, (error, result, fields)=>{
+            res.json({
+                success: result.affectedRows>0
+            })
+    })
+ 
 })
 app.post("/usuarios", (req, res) =>{
     const name= req.body.name
     const lastname= req.body.lastname
     const email= req.body.email
-   
+    const phone= req.body.phone
       
         if(!name || !lastname || !email){
             res.status(400).json({
                 message: "Todos los campos son requeridos"
             })
-        }
-        const id= db.usuarios.length + 1
+        } 
+        else{
     let usuarioNuevo = {
-        id, name, lastname, email
+         name, lastname, email, phone
     }
-    db.usuarios.push(usuarioNuevo)
-    res.json(db.usuarios)
+    connection.query(`INSERT INTO usuarios set ?`, usuarioNuevo, (error, result, fields)=>{
+        res.json({
+            success: result.affectedRows !== 0
+        })
+    
+    })
+    }
 })
 app.put("/usuarios/:idUser", (req, res) =>{
     const idUser= Number(req.params.idUser)
-    const index= db.usuarios.findIndex(usuario => usuario.id === idUser)
+   
 
-    if(index <0){
-        res.status(404).json({
-            message: "No hay datos para actualizar"
-        })
-    }
 const name= req.body.name
 const lastname= req.body.lastname
 const email= req.body.email
+const phone=req.body.phone
 
-    let actualizacionUsuario = {
-        name, lastname, email, id : idUser
-    }
-    db.usuarios[index]= actualizacionUsuario
-    res.json(db.usuarios)
+
+    connection.query(
+        `UPDATE usuarios SEt name=?, lastname=?, email=?, phone=? WHERE id=?`,
+        [name, lastname, email, phone, idUser],
+        (error, result, fields)=>{
+            console.log(error)
+            if(result.affectedRows === 0){
+                res.status(400).json({
+                    message: "We cannot update because the ID was not found",
+                })
+                return
+            }
+            res.json({
+                result
+            })
+        }
+    )
 })
 
 //traer todos los usuarios
 app.get("/providers", (req, res)=>{
-    res.json(db.providers)
+    connection.query(`SELECT * FROM providers`, (error, result, fields)=>{
+        res.json({
+            result
+        })
+    })
 })
 //traer un provider por id
 app.get("/providers/:idprovider", (req, res) => {
     const idProvider = Number(req.params.idprovider)
 
-    if(!idProvider){
-        res.status(400).json({
-            message: "No existe el ID"
-        })
-    }
-    const idsearching= db.providers.find(provider => provider.id ===idProvider)
-    res.json(idsearching)
+    connection.query(`SELECT * FROM providers WHERE id=${idProvider}`, (error, result, fields)=>{
+        
+        if(result.length === 0 ){
+            res.status(404).json({
+                message: "ID not found"
+            })
+            return
+        }
+            res.json({ provider: result[0] })
+    })
+    
 
 })
 //Create new provider
@@ -172,55 +234,45 @@ app.post("/providers", (req, res) => {
         res.status(400).json({
             message: "Todos los datos son requeridos"
         })
-    }
-    const idprovider= db.providers.length + 1
-    
+    }else{
     let newprovider={
-        idprovider, name, phone, email
+             name, phone, email
     }
-    db.providers.push(newprovider)
-    res.json(db.providers)
+        connection.query(`INSERT INTO providers set ?`, newprovider, (error, result, fields)=>{
+           
+            res.json({success: result.affectedRows !== 0})
+        })
+    }  
 })
 //update provider
 app.put("/providers/:idprovider", (req, res) =>{
 
     const idprovider= Number(req.params.idprovider)
-    const index= db.providers.findIndex(provider => provider.id === idprovider)
-
-        if(index< 0){
+    const { name, phone, email } = req.body
+       
+    connection.query(`UPDATE providers set name=?, email=?, phone=? WHERE id=?`,
+    [name, email, phone, idprovider],
+    (error, result, fields)=>{
+    
+    if(result.affectedRows ===0){
             res.status(404).json({
                 message: "No hay datos para actualizar"
             })
+            return
         }
-
-    const { name, phone, email } = req.body
-
-    let updateProvider= {
-        name, phone, email, id: idprovider
-    }
-
-    db.providers[index]= updateProvider
-    res.json(db.providers)
-
-
+        res.json({
+            success: result.affectedRows>0,
+            result
+        })
+     })
 })
 app.delete("/providers/:idprovider", (req, res) => {
     const idProvider= Number(req.params.idprovider)
-    const validationProvider= db.providers.find(provider => provider.id ===idProvider)
-        if(!validationProvider){
-            res.status(404).json({
-                messaje: "El ID no existe"
-            })
-        }
-    
-    db.providers= db.providers.filter(provider => provider.id !== validationProvider.id)
-    res.json(db.providers)
+
+        connection.query(`DELETE FROM providers WHERE id=${idProvider}`, (error, result, fields)=>{
+            res.json({ success: result.affectedRows>0 })
+        })
 })
-
-
-
-
-
 
 app.listen(9000, ()=>{
     console.log("corriendo puerto 9000")
